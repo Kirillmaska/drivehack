f9f0db1a9ec7810ec05f0e4e87c4a9e23581b750
Kirillmaska <kirillmaximov.di@gmail.com>
2022-08-26 15:54:01 +0300

    mm

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_type_structure(format_file, user_id):
         filename=f"{user_id}.{format_file}".format(os.getpid())
         cv2.imwrite(filename, gray)
         text=pytesseract.image_to_string(Image.open(filename),  lang='rus')
-        #os.remove(filename)
+        os.remove(filename)
         if ('фактура' or 'налог') in text.lower():
             return 'Счёт-фактуры'
         elif 'фактура' not in text.lower() and ('счет' or 'счёт' or 'отчет') in text.lower():
