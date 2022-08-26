83a58155848d3e7deb50ed7bc013041346f238a3
Kirillmaska <kirillmaximov.di@gmail.com>
2022-08-26 12:09:25 +0300

    mm

--- main.py
+++ main.py
@@ -1,30 +1,57 @@
-import cv2
 import numpy as np
+import traceback
+from PIL import Image
+import pytesseract
+import cv2
+import os
 
 
-image_file = "images/inv-0003.jpg"
-img = cv2.imread(image_file)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
-
-# Get contours
-contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-output = img.copy()
-
-for idx, contour in enumerate(contours):
-    (x, y, w, h) = cv2.boundingRect(contour)
-    # print("R", idx, x, y, w, h, cv2.contourArea(contour), hierarchy[0][idx])
-    # hierarchy[i][0]: the index of the next contour of the same level
-    # hierarchy[i][1]: the index of the previous contour of the same level
-    # hierarchy[i][2]: the index of the first child
-    # hierarchy[i][3]: the index of the parent
-    if hierarchy[0][idx][3] == 0:
-        cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+def rotate_image(image, angle):
+    (height, width) = image.shape[:2]
+    (cX, cY) = (width / 2, height / 2)
+    matrix = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(matrix[0, 0])
+    sin = np.abs(matrix[0, 1])
+    new_width = int((height * sin) + (width * cos))
+    new_height = int((height * cos) + (width * sin))
+    matrix[0, 2] += (new_width / 2) - cX
+    matrix[1, 2] += (new_height / 2) - cY
+    return cv2.warpAffine(image, matrix, (new_width, new_height))
 
 
-cv2.imshow("Input", img)
-cv2.imshow("Enlarged", img_erode)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+def get_type_structure(format_file, user_id):
+    try:
+        pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+        image=cv2.imread(f'{user_id}.{format_file}')
+        gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        """        x, y, w, h=0, 0, image.shape[1], image.shape[0]
+
+        top_half=((x, y), (x + w, y + h / 2))
+        bottom_half=((x, y + h / 2), (x + w, y + h))
+
+        top_x1, top_y1=top_half[0]
+        top_x2, top_y2=top_half[1]
+        bottom_x1, bottom_y1=bottom_half[0]
+        bottom_x2, bottom_y2=bottom_half[1]
+        blurred=cv2.GaussianBlur(gray, (3, 3), 0)
+        thresh=cv2.threshold(blurred, 110, 255, cv2.THRESH_BINARY_INV)[1]
+        top_image=thresh[top_y1:top_y2, top_x1:top_x2]
+        bottom_image=thresh[bottom_y1:bottom_y2, bottom_x1:bottom_x2]
+
+        top_pixels=cv2.countNonZero(top_image)
+        bottom_pixels=cv2.countNonZero(bottom_image)
+        if top_pixels > bottom_pixels:
+            gray=rotate_image(gray, 180)
+        """
+        filename=f"{user_id}.{format_file}".format(os.getpid())
+        cv2.imwrite(filename, gray)
+        text=pytesseract.image_to_string(Image.open(filename),  lang='rus')
+        os.remove(filename)
+        if ('фактура' or 'налог') in text.lower():
+            return 'Счёт-фактуры'
+        elif 'фактура' not in text.lower() and ('счет' or 'счёт' or 'отчет') in text.lower():
+            return 'Счёт'
+        else:
+            return 'Документ не распознан. Ошибка'
+    except Exception:
+        print(traceback.format_exc())
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import cv2
+from main import get_type_structure
+import datetime
 
-img = cv2.imread('images/inv-0003.jpg')
-img = cv2.resize(img, (img.shape[1] *2, img.shape[0] * 2))
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-print(img.shape)
-cv2.imshow('Result', img)
-cv2.waitKey(0)
-cv2.imwrite('kek.jpg', img)
+
+t1 = datetime.datetime.now().time()
+
+c = 0
+while c != 300:
+    print(get_type_structure('jpg', 'images/inv-000')
